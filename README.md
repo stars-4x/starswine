cedfd2af0adb975fc8068a9e9ae8f2cee9eedf77
raptor <buckyballreaction@gmail.com>
2019-06-13 20:41:25 -0600

    Update to use otvdm

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # starswine
-This project uses BoxedWine (http://www.boxedwine.org/) to run Stars! in Windows 10 64-bit
+Run Stars! in Windows 64-bit.
+
+This project uses winevdm (also known as otvdm) from: https://github.com/otya128/winevdm/
--- stars.bat
+++ stars.bat
@@ -1,12 +1,8 @@
 rem Change to the directory this script is in
 cd "%cd%"
 
-set working_dir=/home/username/stars
-set exe=stars.exe
-set wine_args=explorer /desktop=stars
+set launcher=.\otvdm\otvdm.exe
+set exe=.\stars\stars.exe
 
-set resolution=1024x768
-set ram=64
-
-Boxedwine -root filesystem -m %ram% -resolution %resolution% -w %working_dir% /bin/wine %wine_args% %working_dir%/%exe%
-pause
+rem Run the system!
+%launcher% %exe%
